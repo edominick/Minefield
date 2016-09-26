dd0036bdb22d6df1cca85294c1ff4cd1d6559744
Erina Dominick <edominick@mail.csuchico.edu>
2016-09-26 11:56:10 -0700

    first field tests and buglog

--- FieldTest.cpp
+++ FieldTest.cpp
@@ -22,3 +22,20 @@ TEST(FieldTest, placeMineInBounds)
 	minefield.placeMine(4,5);
 	ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
 }
+
+TEST(FieldTest, placeMineOutBounds)
+{
+	Field minefield;
+
+	minefield.placeMine(-3.3,5);
+	ASSERT_EQ( MINE_HIDDEN, minefield.get(-3.3,5) );	
+}
+
+TEST(FieldTest, isSafeOutBounds)
+{
+	Field minefield;
+
+	minefield.placeMine(-3.3,5);
+	minefield.isSafe(-3.3, 5);
+	ASSERT_EQ( MINE_HIDDEN, minefield.get(-3.3,5) );
+}
--- buglog.md
+++ buglog.md
@@ -1,1 +1,7 @@
 # BUG LOG
+
+1 - placeMineOutBounds
+	places a mine in an area that doesnt exist, such as -3.3 when the dimensions of the grid are 10x10
+
+2 - isSafeOutBounds
+	checks the isSafe function by setting the mine to a location that doesnt exist 
